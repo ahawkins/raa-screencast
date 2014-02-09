2f5685d7165aea39271cf2dd821ff009cab993d1
ahawkins <adam@hawkins.io>
2014-02-09 22:12:24 +0100

    WebService uses hash initializer

--- web.rb
+++ web.rb
@@ -2,14 +2,8 @@ require 'sinatra'
 
 class WebService < Sinatra::Application
   post '/invoices' do
-    form = SendInvoiceForm.new
-
-    form.name = params['invoice']['name']
-    form.email = params['invoice']['email']
-    form.description = params['invoice']['description']
-    form.amount = params['invoice']['amount']
-
-    use_case  = SendInvoice.new form
+    form = SendInvoiceForm.new params['invoice']
+    use_case = SendInvoice.new form
 
     use_case.run!
 
